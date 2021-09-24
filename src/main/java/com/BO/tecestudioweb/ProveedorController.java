7136b0576e75dc15d44c23eca646d122117e01e3
SergioHGHomez <90337735+SergioHGHomez@users.noreply.github.com>
2021-09-23 21:30:06 -0500

    back crud proveedores terminado

--- src/main/java/com/BO/tecestudioweb/ProveedorController.java
+++ src/main/java/com/BO/tecestudioweb/ProveedorController.java
@@ -1,5 +1,6 @@
 package com.BO.tecestudioweb;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -42,16 +43,16 @@ public class ProveedorController {
 	}
 	
 	@GetMapping("/proveedorGuardar")
-	public Map<String, Boolean> buscarProveedor(){
+	public Map<String, Boolean> guardarProveedor(){
 		
 		boolean guardado = false;
 		ProveedorVO proveedor = new ProveedorVO();
-		proveedor.setNit_Proveedor(10000000);
-		proveedor.setNom_proveedor("TestSave");
+		proveedor.setNit_Proveedor(101);
+		proveedor.setNom_proveedor("TestSave2");
 		proveedor.setEmail_proveedor("TestProveedor@mail.com");
 		proveedor.setCiudad_proveedor("BOGOTA");
 		proveedor.setDirec_proveedor("cll 7 No 21b 80");
-		proveedor.setTel_proveedor("7777777");
+		proveedor.setTel_proveedor("4444444");
 		
 		guardado = proveedorService.guardar(proveedor);
 		
@@ -61,10 +62,58 @@ public class ProveedorController {
 		}else {
 			return Collections.singletonMap("guardado", false);		}
 		
+	}
 	
+
+	@RequestMapping("/proveedorBuscar")
+	public Map<Integer, ProveedorVO> buscarProveedor() {
+		List<ProveedorVO> encontrados = new ArrayList<ProveedorVO>();
+		Map<Integer, ProveedorVO> proveedores = new HashMap<Integer, ProveedorVO>();
+		int contador = 0;
+		
+		encontrados = proveedorService.buscar(9008315524l);
+		
+		for (ProveedorVO proveedor : encontrados) {
+			proveedores.put(contador, proveedor);
+			contador++;
+		}
+		return proveedores;
+
+	}
+	
+	@GetMapping("/proveedorEliminar")
+	public Map<String, Boolean> eliminarProveedor(){
+		
+		boolean eliminado = proveedorService.eliminar(101);
+		
+		if(eliminado) {
+			return Collections.singletonMap("eliminado", true);
+		}else {
+			return Collections.singletonMap("eliminado", false);
+					
+		}
+		
 		
 	}
-
-	
+	@GetMapping("/proveedorActualizar")
+	public Map<String, Boolean> actualizarProveedor(){
+		boolean actualizado = false;
+		ProveedorVO proveedorGuardar = new ProveedorVO();
+		proveedorGuardar.setNit_Proveedor(101);
+		proveedorGuardar.setNom_proveedor("testUpdate sas");
+		proveedorGuardar.setEmail_proveedor("update@mail.com");
+		proveedorGuardar.setDirec_proveedor("Cl. 10 ##20-35");
+		proveedorGuardar.setCiudad_proveedor("Bogota");
+		proveedorGuardar.setTel_proveedor("2222222");
+		actualizado = proveedorService.actualizar(proveedorGuardar);
+		
+		if(actualizado) {
+			return Collections.singletonMap("actualizado", true);
+		}else {
+			return Collections.singletonMap("actualizado", false);
+		}
+		
+		
+	}
 	
 }
--- src/main/java/com/DAO/tecestudioweb/ProveedorDAO.java
+++ src/main/java/com/DAO/tecestudioweb/ProveedorDAO.java
@@ -17,7 +17,7 @@ public class ProveedorDAO {
 	public List< ProveedorVO > traerTodos(){
 		Conexion conexion = new Conexion();
 		String query = "select * from proveedor";
-		
+		 
 		ProveedorVO proveedorTemp ;
 		List<ProveedorVO> listaProveedores = new ArrayList<ProveedorVO>();
 		
@@ -34,15 +34,15 @@ public class ProveedorDAO {
 					
 					proveedorTemp.setNit_Proveedor(result.getLong("nit_proveedor"));
 					proveedorTemp.setNom_proveedor(result.getString("nom_proveedor"));
-					proveedorTemp.setEmail_proveedor(result.getString("ciudad_proveedor"));
-					proveedorTemp.setCiudad_proveedor(result.getString("nom_proveedor"));
+					proveedorTemp.setEmail_proveedor(result.getString("email_proveedor"));
+					proveedorTemp.setCiudad_proveedor(result.getString("ciudad_proveedor"));
 					proveedorTemp.setDirec_proveedor(result.getString("direc_proveedor"));
 					proveedorTemp.setTel_proveedor(result.getString("tel_proveedor"));
 					
 					listaProveedores.add(proveedorTemp);
 					
 				}
-				conn.close();
+				conexion.desconectar();
 			}
 			
 		} catch (Exception e) {
@@ -87,8 +87,109 @@ public class ProveedorDAO {
 		}
 	
 	//-------------------- metodo buscar ------------------------------------
+		public List<ProveedorVO> buscar (Long nit){
+			ProveedorVO proveedorTemp = null;
+			List<ProveedorVO> proveedores = new ArrayList<ProveedorVO>();
+			String query = "select * from proveedor where nit_proveedor = ?";
+			Conexion conexion = new Conexion();
+			
+			try {
+				Connection conn = conexion.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(query);
+				pstmt.setLong(1, nit);
+				ResultSet result = pstmt.executeQuery();
+				
+				if(result != null) {
+					
+					while(result.next()){
+						
+						proveedorTemp = new ProveedorVO();
+						proveedorTemp.setNit_Proveedor(result.getLong("nit_proveedor"));
+						proveedorTemp.setNom_proveedor(result.getString("nom_proveedor"));
+						proveedorTemp.setEmail_proveedor(result.getString("email_proveedor"));
+						proveedorTemp.setCiudad_proveedor(result.getString("ciudad_proveedor"));
+						proveedorTemp.setDirec_proveedor(result.getString("direc_proveedor"));
+						proveedorTemp.setTel_proveedor(result.getString("tel_proveedor"));
+						proveedores.add(proveedorTemp);
+					}
+					conexion.desconectar();
+				}
+				
+			} catch (Exception e) {
+				System.out.println("no fue posible buscar al proveedor " +e);
+			}
+			return proveedores ;
+		}
+		
+		
 	//-------------------- motodo eliminar ----------------------------------
+		public boolean eliminar(long nit) {
+			String query = "delete from proveedor where nit_proveedor = ?";
+			int respuesta = 0;
+			boolean eliminado = false;
+			Conexion conexion = new Conexion();
+			
+			try {
+				Connection conn = conexion.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(query);
+				pstmt.setLong(1, nit);
+				respuesta = pstmt.executeUpdate();
+				if(respuesta > 0) {
+					eliminado = true;
+				}
+				
+			} catch (Exception e) {
+				System.out.println("no se logro eliminar al proveedor "+e);
+			}finally {
+				
+				try {
+					conexion.desconectar();
+				} catch (Exception e2) {
+					// TODO: handle exception
+				}
+			}
+			
+			
+			
+			return eliminado;
+		}
+		
 	//-------------------- motoda actualizar --------------------------------
-	
+	public boolean actualizar(ProveedorVO proveedor ){
+		String query = "update proveedor "
+				+ "set nom_proveedor = ?, "
+				+ "email_proveedor = ?, "
+				+ "ciudad_proveedor = ?, "
+				+ "direc_Proveedor = ?, "
+				+ "tel_proveedor = ? "
+				+ "where nit_proveedor = ?";
+		int resultado = 0;
+		Conexion conexion = new Conexion();
+		boolean actualizado = false;
+		try {
+			Connection conn = conexion.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, proveedor.getNom_proveedor());
+			pstmt.setString(2, proveedor.getEmail_proveedor());
+			pstmt.setString(3, proveedor.getCiudad_proveedor());
+			pstmt.setString(4, proveedor.getDirec_proveedor());
+			pstmt.setString(5, proveedor.getTel_proveedor());
+			pstmt.setLong(6, proveedor.getNit_Proveedor());
+			resultado = pstmt.executeUpdate();
+			if(resultado>0){
+				actualizado = true;
+			}
+		} catch (Exception e) {
+			System.out.println("no se pudo actualizar "+e);
+		}finally {
+			try {
+				conexion.desconectar();
+			} catch (Exception e2) {
+				// TODO: handle exception
+			}
+		}
+		
+		return actualizado;
+	}
 
 }
